3897079da67558c6c0e87a10f8ddea29b9186ba7
Larguma <larguma@hotmail.fr>
2023-04-25 14:56:30 +0200

    add script to dynamicaly change waybar theme

---
+++ Hyprland/waybar/change_theme.sh
@@ -0,0 +1,37 @@
+#/bin/sh
+
+##################################################
+# script to change the waybar theme for Hyprland #
+##################################################
+
+# package nedded:
+#   extra/zenity
+
+# config:
+#  put all your themes folders under '~/.config/waybar/themes'
+#  ex: ~/.config/waybar/themes/theme1
+#      ~/.config/waybar/themes/theme2
+
+# usage:
+#   bind this script to a key in Hyprland 
+#   or make a custom module in the waybar config
+
+for dir in $HOME/.config/waybar/themes/*/;
+do
+  buffer=$(basename "$dir")
+  themes="${themes} ${buffer}"
+done
+
+userInput=$(zenity --list \
+  --title="Choose your waybar theme" \
+  --column="Theme folder" \
+  $themes)
+
+rm $HOME/.config/waybar/config.jsonc
+rm $HOME/.config/waybar/style.css
+ln $HOME/.config/waybar/themes/$userInput/config.jsonc $HOME/.config/waybar/config.jsonc
+ln $HOME/.config/waybar/themes/$userInput/style.css $HOME/.config/waybar/style.css
+
+pkill waybar
+
+hyprctl dispatch exec waybar
